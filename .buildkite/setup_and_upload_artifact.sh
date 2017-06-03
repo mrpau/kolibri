3797f95dc4cb3f2611b284938fddb7525b34cb17
Eduard James Aban <eduard.aban@mrpau.com>
2017-06-04 03:25:27 +0800

    no message

--- .buildkite/setup_and_upload_artifact.sh
+++ .buildkite/setup_and_upload_artifact.sh
@@ -31,6 +31,11 @@ fi
 
 PYTHON_CMD="$PYTHON_PATH .buildkite/upload_artifacts.py"
 echo "Now excuting  upload artifacts script..."
+mkdir dist
+buildkite-agent artifact download 'dist/*.pex' dist/
+buildkite-agent artifact download 'dist/*.whl' dist/
+buildkite-agent artifact download 'dist/*.zip' dist/
+buildkite-agent artifact download 'dist/*.tar.gz' dist/
 $PYTHON_CMD
 if [ $? -ne 0 ]; then
     echo ".. Abort!  Can't execute '$PYTHON_CMD'."
